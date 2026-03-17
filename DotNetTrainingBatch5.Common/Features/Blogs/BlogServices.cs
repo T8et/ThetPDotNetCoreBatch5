67752d945306839c113a53b0f7f1c9fe1ef54289
T8et <devt8et2022@gmail.com>
2026-03-17 23:04:11 +0630

    feat : blog services controller add

---
+++ DotNetTrainingBatch5.Common/Features/Blogs/BlogServices.cs
@@ -0,0 +1,87 @@
+﻿using DotNetTrainingBatch5.Database.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetTrainingBatch5.Common.Features.Blogs
+{
+    public class BlogServices
+    {
+        protected readonly AppDBContext _db = new AppDBContext();
+
+        public List<TblBlog> getBlogs()
+        {
+            var data = _db.TblBlogs.AsNoTracking().ToList();
+            return data;
+        }
+
+        public List<TblBlog> getBlog(int id)
+        {
+            var data = _db.TblBlogs.AsNoTracking().Where(x=>x.BlogId == id).ToList();
+            return data;
+        }
+
+        public TblBlog createBlog(TblBlog data_blog)
+        {
+            _db.Add(data_blog);
+            _db.SaveChanges();
+            return data_blog;
+        }
+
+        public TblBlog updateBlog(int id, TblBlog data_blog)
+        {
+            var itemdata = _db.TblBlogs.AsNoTracking().FirstOrDefault(_ => _.BlogId == id);
+            if (itemdata is null) return null;
+
+            itemdata.BlogTitle = data_blog.BlogTitle;
+            itemdata.BlogAuthor = data_blog.BlogAuthor;
+            itemdata.BlogContent = data_blog.BlogContent;
+
+            _db.Entry(itemdata).State = EntityState.Modified;
+            _db.SaveChanges();
+
+            return data_blog;
+        }
+
+        public TblBlog patchBlog(int id, TblBlog data_blog)
+        {
+            var itemdata = _db.TblBlogs.AsNoTracking().FirstOrDefault(_ => _.BlogId == id);
+            if (itemdata is null) return null;
+
+            if (data_blog.BlogTitle is not null)
+            {
+                itemdata.BlogTitle = data_blog.BlogTitle;
+            }
+            if (data_blog.BlogAuthor is not null)
+            {
+                itemdata.BlogAuthor = data_blog.BlogAuthor;
+            }
+            if (data_blog.BlogContent is not null)
+            {
+                itemdata.BlogContent = data_blog.BlogContent;
+            }
+
+            _db.Entry(itemdata).State = EntityState.Modified;
+            _db.SaveChanges();
+
+            return data_blog;
+        }
+
+        public TblBlog deleteBlog(int id)
+        {
+            var itemdata = _db.TblBlogs.AsNoTracking().FirstOrDefault(_ => _.BlogId == id);
+            if (itemdata is null) return null;
+
+            itemdata.DeleteFlag = true;
+
+            _db.Entry(itemdata).State = EntityState.Modified;
+            _db.SaveChanges();
+
+            return itemdata;
+        }
+    }
+}
---
+++ DotNetTrainingBatch5.RESTAPI/Controllers/BlogServicesController.cs
@@ -0,0 +1,80 @@
+﻿using DotNetTrainingBatch5.Common.Features.Blogs;
+using DotNetTrainingBatch5.Database.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetTrainingBatch5.RESTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogServicesController : ControllerBase
+    {
+        private readonly BlogServices _service;
+
+        public BlogServicesController()
+        {
+            _service = new BlogServices();
+        }
+
+        public IActionResult GetBlogs()
+        {
+            var list = _service.getBlogs();
+            return Ok(list);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBlogs(int id)
+        {
+            var list = _service.getBlog(id);
+            if (list is null)
+            {
+                return BadRequest();
+            }
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog(TblBlog tblBlog)
+        {
+            _service.createBlog(tblBlog);
+            return Ok(tblBlog);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBlog(int id, TblBlog tblblog)
+        {
+            var list = _service.updateBlog(id, tblblog);
+            if (list is null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(list);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult PatchBlog(int id, TblBlog tblBlog)
+        {
+            var list = _service.patchBlog(id, tblBlog);
+            if (list is null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(list);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBlog(int id)
+        {
+            var list = _service.deleteBlog(id);
+            if (list is null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(list);
+        }
+    }
+}
