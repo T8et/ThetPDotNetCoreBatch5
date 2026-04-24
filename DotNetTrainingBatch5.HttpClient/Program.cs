3c2f5d24cb019fded93e5742cd8be618fa5f09d5
T8et <devt8et2022@gmail.com>
2026-04-24 23:27:32 +0630

    feat : HttpClient Conso App Initial

---
+++ DotNetTrainingBatch5.HttpClient/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DotNetTrainingBatch5.ConsoClient.Services;
+
+Console.WriteLine("Hello, World!");
+
+ResultResponse response = new ResultResponse();
+await response.Read();
+await response.ReadById(102);
+
---
+++ DotNetTrainingBatch5.HttpClient/Services/ResultResponse.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetTrainingBatch5.ConsoClient.Services;
+
+public class ResultResponse
+{
+    private readonly HttpClient _http;
+    private readonly string EndPoints = "https://jsonplaceholder.typicode.com/posts";
+    public ResultResponse()
+    {
+        _http = new HttpClient();
+    }
+    
+    public async Task Read()
+    {
+        var res = await _http.GetAsync(EndPoints);
+
+        if (res.IsSuccessStatusCode)
+        {
+            string jsonRes = await res.Content.ReadAsStringAsync();
+            Console.WriteLine(jsonRes);
+        }
+        else
+        {
+            Console.WriteLine("Invalid Request");
+        }
+    }
+
+    public async Task ReadById(int id)
+    {
+        var res = await _http.GetAsync($"{EndPoints}/{id}");
+
+        if (res.IsSuccessStatusCode)
+        {
+            string jsonRes = await res.Content.ReadAsStringAsync();
+            Console.WriteLine(jsonRes);
+        }
+        else
+        {
+            Console.WriteLine("Invalid Request");
+        }
+    }
+}
