975690cd6379a42ff606c53109f767917968d3a9
T8et <devt8et2022@gmail.com>
2026-04-25 17:54:02 +0630

    feat : update restshar[

--- DotNetTrainingBatch5.HttpClient/Program.cs
+++ DotNetTrainingBatch5.HttpClient/Program.cs
@@ -3,8 +3,12 @@ using DotNetTrainingBatch5.ConsoClient.Services;
 
 Console.WriteLine("Hello, World!");
 
-ResultResponse response = new ResultResponse();
-await response.Read();
+//ResultResponse response = new ResultResponse();
+//await response.Read();
+
+RestSharpExample example = new RestSharpExample();
+//await example.Read();
+await example.ReadById(9);
 //await response.ReadById(102);
 //await response.Update(100, 1, "hello", "hello test");
 //await response.Delete(10);
