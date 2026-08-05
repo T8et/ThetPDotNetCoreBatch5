a900bcbd52408f0a85d434e37a112f4e3752312a
T8et <devt8et2022@gmail.com>
2026-08-05 23:08:44 +0630

    feat : encrypt , decrypt

---
+++ DotNetTrainingBatch5.EncDec/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Effortless.Net.Encryption;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+byte[] key = Encoding.UTF8.GetBytes("thisisasecretkey");
+byte[] iv = Encoding.UTF8.GetBytes("thisisasecretkey");
+
+string encrypted = Strings.Encrypt("kempoSecret", key, iv);
+string decrypted = Strings.Decrypt(encrypted, key, iv);
+
+Console.WriteLine("encrypted >> "+encrypted);
+Console.WriteLine("decrypted >> "+decrypted);
+Console.Read();
