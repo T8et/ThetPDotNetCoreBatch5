5334e133e452eedc3876e1b63c29ddd3ca05689b
T8et <devt8et2022@gmail.com>
2026-08-09 23:37:41 +0630

    feat : login encrypt flow

---
+++ DotNetTrainingBatch5.LoginFlowEncrypt/Controllers/BlogController.cs
@@ -0,0 +1,91 @@
+﻿using DotNetTrainingBatch5.LoginFlowEncrypt.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace DotNetTrainingBatch5.LoginFlowEncrypt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly EncDecSrvc encDecSrvc;
+
+        public BlogController(EncDecSrvc encDecSrvc)
+        {
+            this.encDecSrvc = encDecSrvc;
+        }
+
+        [HttpPost]
+        public IActionResult Login(loginRequest login)
+        {
+            try
+            {
+                var result = userStore.users.Where(x => x.username == login.username && x.password == login.password).FirstOrDefault();
+                if (result == null)
+                {
+                    return Unauthorized();
+                }
+                else
+                {
+                    var user = new loginModel
+                    {
+                        sessionExpireTime = DateTime.Now.AddMinutes(1),
+                        sessionId = Guid.NewGuid().ToString(),
+                        username = result.username
+                    };
+
+                    var jsonstring = JsonConvert.SerializeObject(user);
+                    var token = encDecSrvc.encryptData(jsonstring);
+
+                    var model = new blogResponseModel
+                    {
+                        accessToken = token
+                    };
+
+                    return Ok(model);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.ToString());
+            }
+        }
+    }
+
+    public static class userStore
+    {
+        public static List<userData> users = new List<userData>()
+        {
+            new userData(){username="admin",password="admin"},
+            new userData(){username="user1",password="user1"},
+            new userData(){username="user2",password="user2"},
+        };
+    }
+
+    public class  loginRequest
+    {
+        public string? username { get; set; }
+        public string? password { get; set; }
+    }
+
+    public class userData
+    {
+        public string? username { get; set; }
+        public string? password { get; set; }
+    }
+
+    public class loginModel
+    {
+        public string? username { get; set; }
+        public string? sessionId { get; set; }
+
+        public DateTime? sessionExpireTime { get; set; }
+    }
+
+    public class blogResponseModel
+    {
+        public string? accessToken { get; set; }
+    }
+}
---
+++ DotNetTrainingBatch5.LoginFlowEncrypt/Services/EncDecSrvc.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using Effortless.Net.Encryption;
+
+namespace DotNetTrainingBatch5.LoginFlowEncrypt.Services
+{
+    public class EncDecSrvc
+    {
+        private readonly byte[] key;
+        private readonly byte[] iv;
+        public EncDecSrvc(IConfiguration configuration)
+        {
+            key = Encoding.ASCII.GetBytes(configuration["Security:keycode"]!);
+            iv = Encoding.ASCII.GetBytes(configuration["Security:ivcode"]!);
+        }
+
+        public string encryptData(string plainText)
+        {
+            return Strings.Encrypt(plainText, key, iv);
+        }
+
+        public string decryptData(string plainText)
+        {
+            return Strings.Decrypt(plainText, key, iv);
+        }
+    }
+}
